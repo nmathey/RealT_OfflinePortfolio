150a8f445632fa9e3e8594a4d8193b7a657b56c3
nmathey <20896232+nmathey@users.noreply.github.com>
2023-02-05 15:50:48 +1100

    Improving frontend result

--- MyPortfolio.js
+++ MyPortfolio.js
@@ -8,28 +8,37 @@ async function populate(){
 }
 
 function populateProperties(jsonObj) {
-    const portfolio = document.querySelector('portfolio');
+    const portfolio = document.getElementById('portfolio');
     const properties = jsonObj;
-    console.log(properties);
 
     for (const property in properties) {
-        const myProperty = document.createElement('property');
-        myProperty.classList.add('property-layout');
-        const name = document.createElement('h2');
-        const tokenAmount = document.createElement('p');
-        const currentValuation = document.createElement('p');
-        const currentRentedUnitP = document.createElement('p');
-        const currentNetRentP = document.createElement('p');
+        const myProperty = document.createElement('tr');
+        const name = document.createElement('td');
+        const tokenAmount = document.createElement('td');
+        const currentValuation = document.createElement('td');
+        const investedValue = document.createElement('td');
+        const currentRentedUnitP = document.createElement('td');
+        const currentNetRentP = document.createElement('td');
 
         name.textContent = properties[property].Shortname;
-        tokenAmount.textContent = 'Token owned: ' + properties[property].CurrentBalance.toFixed(5);
-        currentValuation.textContent = 'Current value: ' + properties[property].CurrentValue.toFixed(2) + ' (/w ' + properties[property].InvestValue + ' invested)';
-        currentRentedUnitP.textContent = 'Rented at ' + properties[property].CurrentRentedUnitsP;
-        currentNetRentP.textContent = 'Current net rent ratio is ' + properties[property].CurrentNetRentP;
+        tokenAmount.textContent = properties[property].CurrentBalance.toFixed(5);
+        currentValuation.textContent = properties[property].CurrentValue.toFixed(2);
+        if (properties[property].CurrentValue.toFixed(2) < properties[property].InvestValue.toFixed(2)) {
+             currentValuation.style.backgroundColor = "red";
+        } else if (properties[property].CurrentValue.toFixed(2) > properties[property].InvestValue.toFixed(2)){
+            currentValuation.style.backgroundColor = "green";
+        }
+        investedValue.textContent = properties[property].InvestValue.toFixed(2);
+        currentRentedUnitP.textContent = (/\d+ ?/).exec(properties[property].CurrentRentedUnitsP) + '%';
+        if ((/\d+ ?/).exec(properties[property].CurrentRentedUnitsP) < 100) {
+             currentRentedUnitP.style.backgroundColor = "red";
+        }
+        currentNetRentP.textContent = properties[property].CurrentNetRentP;
 
         myProperty.appendChild(name);
         myProperty.appendChild(tokenAmount);
         myProperty.appendChild(currentValuation);
+        myProperty.appendChild(investedValue);
         myProperty.appendChild(currentRentedUnitP);
         myProperty.appendChild(currentNetRentP);
 
